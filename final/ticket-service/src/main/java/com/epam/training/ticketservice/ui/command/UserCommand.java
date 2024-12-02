57a80fb12b808f7792fd313b41ab84dacf666030
Tóth Dániel <tothdani561@gmail.com>
2024-12-02 11:24:47 +0100

    "describe account" command created

--- final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/UserCommand.java
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/UserCommand.java
@@ -13,12 +13,27 @@ public class UserCommand {
     private final UserService userService;
 
     @ShellMethod(key = "sign in privileged", value = "Sign in as privileged user")
-    public String signInPrivileged(String username, String password) {
+    public String signIn(String username, String password) {
         userService.logout();
         return userService.login(username, password)
                 .filter(userDto -> userDto.role() == User.Role.ADMIN) // Csak ADMIN role engedélyezett
                 .map(userDto -> "Admin user " + userDto.username() + " is successfully logged in!")
                 .orElse("Login failed due to incorrect credentials");
+    }
+
+    @ShellMethod(key = "sign out", value = "Sign out")
+    public String signOut() {
+        return userService.logout()
+                .map(userDto -> userDto.username() + " is logged out!")
+                .orElse("You need to login first!");
+    }
+
+    @ShellMethod(key = "describe account", value = "Describe the currently signed-in account")
+    public String describeAccount() {
+        return userService.describe()
+                .map(userDto -> "Signed in with " + (userDto.role() == User.Role.ADMIN ? "privileged" : "standard")
+                        + " account '" + userDto.username() + "'")
+                .orElse("You are not signed in");
     }
 
     @ShellMethod(key = "admin task", value = "Perform an admin-only task")
